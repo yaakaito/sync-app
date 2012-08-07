1bfc9cb1c89036f05fa6dfd6be3c88d4d9243955
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-08 04:52:58 +0900

    backboneなオブジェクトを関連づけて表示

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,32 @@
 function AppCtrl($scope) {
+
+  var SyncAppObject = Parse.Object.extend("SyncAppObject", {
+    default: {
+      description: "description"
+    }
+  });
+  var SyncAppObjectList = Parse.Collection.extend({
+    model: SyncAppObject
+    , comparator : function(obj) {
+      return obj.get("name");
+    }
+  });
+  
+  var list = new SyncAppObjectList();
+  $scope.items = list.models;
+
   $scope.createAccount = function() {
   
-  }
+  };
 
   $scope.login = function() {
 
-  }
+  };
 
   $scope.addItem = function() {
-    
-  }
+    var item = new SyncAppObject({name : $scope.itemName, description : $scope.itemDescription});
+    list.add(item);    
+  };
 
   Parse.initialize("ZmZd8uAiLo0jYP6rQmxZ4YyBprNVDg2hZqUoGane", "Etil6taJbynJCtWn5OlRvQ4t2pKmufwocoxTbxJg");
 
