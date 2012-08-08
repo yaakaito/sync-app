127226a367565cccb6cdf11c996d2888b5ee02f8
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-09 01:30:54 +0900

    ログインとデータへの関連づけ

--- javascripts/app.js
+++ javascripts/app.js
@@ -18,6 +18,8 @@ function AppCtrl($scope) {
   $scope.createAccount = function() {
     Parse.User.signUp($scope.naccount, $scope.npass, { ACL: new Parse.ACL() }, {
       success: function(user) {
+        $scope.loginUser = Parse.User.current().get("username");
+        $scope.$apply();
         alert("ユーザー登録に成功したよ。 o(*^▽^*)o");
       }
       , error: function(user, error) {
@@ -27,15 +29,36 @@ function AppCtrl($scope) {
   };
 
   $scope.login = function() {
-
+    Parse.User.logIn($scope.laccount, $scope.lpass, {
+      success: function(user) {
+        $scope.loginUser = Parse.User.current().get("username");
+        $scope.$apply();
+        alert("ログインに成功したよ。 o(*^▽^*)o");
+      },
+      error: function(user, error) {
+        alert("ログインに失敗しちゃったよ。 (ﾉ_･｡)");
+      }
+    });
   };
 
   $scope.addItem = function() {
-    var item = new SyncAppObject({name : $scope.itemName, description : $scope.itemDescription});
+    if(!Parse.User.current()) {
+      alert("ログインするかユーザーつくってね。ヾ(@~▽~@)ノ");
+      return ;
+    }
+    var item = new SyncAppObject({name : $scope.itemName
+                                  , description : $scope.itemDescription
+                                  , user : Parse.User.current()
+                                  , ACL : new Parse.ACL(Parse.User.current())});
     list.add(item);
-    item.save();   
+    $scope.items = list.models;
+    item.save();
   };
 
   Parse.initialize("ZmZd8uAiLo0jYP6rQmxZ4YyBprNVDg2hZqUoGane", "Etil6taJbynJCtWn5OlRvQ4t2pKmufwocoxTbxJg");
-
+  if(Parse.User.current()) {
+    $scope.loginUser = Parse.User.current().get("username");
+  } else {
+    $scope.loginUser = "not login";
+  }
 }
